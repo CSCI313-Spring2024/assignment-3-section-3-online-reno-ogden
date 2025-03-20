eec59009cf17c6189d7d769e096f0a17aea46f9f
reno-ogden <reno.ogden02@gmail.com>
2025-03-19 23:45:46 -0500

    Integrated details page into the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
